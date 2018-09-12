f2218a4dbd9011aff26cca8872905a30ed595004
ilyakukarkin <ilya1196@yandex.ru>
2018-09-12 11:41:01 +0500

    fix reload page error with express server

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,11 @@ let path = require('path');
 let serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
 let port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
